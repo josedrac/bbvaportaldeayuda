ed0306fd6caa0fe7b161c54a26a229128e117c03
drac.jose <drac.jose@gmail.com>
2013-12-03 15:32:27 +0000

    [fase2] sideBar js

--- fase2/base_lateral_menu.js
+++ fase2/base_lateral_menu.js
@@ -1,76 +1,82 @@
-console.log(7);
-var gadgetHelper = null;
-var prefs = new _IG_Prefs();
-var urlTable = prefs.getString("_table_query_url");
-var subseccion = prefs.getString("subseccion"); 
-_IG_RegisterOnloadHandler(loadVisualizationAPI);
-
-function loadVisualizationAPI() {
-  google.load("visualization", "1", {"packages": ["table"]});
-}
+var sidebar = function(options){
+  var url_detail = options.url_detail || 'https://sites.google.com/a/bbva.com/portaldeayuda/movilidad/detalle_app?app_id=';
+  var fields = options.fields || { id : 0, name: 1,image: 3 } ;
+  var template = options.template || function(dataDCm, subseccion){
+       var html= "";
+       html +='<div class=\"titulo\">' + subseccion + '</div>';
+         html +='<ul id=\"botonera\">'; 
+         for(var row=0;row<filas;row++){
+           if( dataDC.validateRow(row)){
+              html +="<li><a  href='"+dataDC.getHref(row)+"' target='_parent'>"+
+              dataDC.getName(row)+"</a></li>";
+           }
+        }
+    
+      html +='</ul>';
+      return html;
+  }
   
-function devicesCountry(pais){
-  var gadgetHelper = new google.visualization.GadgetHelper();
-  var opts = {dataType:'jsonp'};
-  var queryDC = new google.visualization.Query(urlTable, opts);
-   
-  var queryDCtosend = "select *";
-
-  queryDC.setQuery(queryDCtosend);			  
-  queryDC.send(handleQueryResponseDC);
-
-}
-
-var DataTable= function(data){
-  var HEADER_ROW = 0;
-  var FIELDS = {
-     id : 0,
-     name: 1,
-     image: 3
-  };
-
-  var dataDC = data;
-  this.getId = function(row){
-     return dataDC.getValue(row, FIELDS.id);
+  
+  var gadgetHelper = null;
+  var prefs = new _IG_Prefs();
+  var urlTable = prefs.getString("_table_query_url");
+  var subseccion = prefs.getString("subseccion"); 
+  _IG_RegisterOnloadHandler(loadVisualizationAPI);
+  
+  function loadVisualizationAPI() {
+    google.load("visualization", "1", {"packages": ["table"]});
   }
-  this.getHref = function(row){
-     return 'https://sites.google.com/a/bbva.com/portaldeayuda/movilidad/detalle_app?app_id='+this.getId(row);
+    
+  function devicesCountry(pais){
+    var gadgetHelper = new google.visualization.GadgetHelper();
+    var opts = {dataType:'jsonp'};
+    var queryDC = new google.visualization.Query(urlTable, opts);
+     
+    var queryDCtosend = "select *";
+  
+    queryDC.setQuery(queryDCtosend);			  
+    queryDC.send(handleQueryResponseDC);
+  
   }
-  this.getNumberOfRows = function(){
-     return dataDC.getNumberOfRows();
-  }
-  this.getName = function(row){
-     return dataDC.getValue(row, FIELDS.name);
-  }		
-  this.getImage = function(row){
-     return dataDC.getValue(row, FIELDS.image);
-  }
-
-  this.validateRow = function(row){
-     return true;
-  }
-
-}
-
-function handleQueryResponseDC(response){
-   var dataDC = new DataTable(response.getDataTable());
-  var filas = dataDC.getNumberOfRows();
-
-     var html= "";
-   html +='<div class=\"titulo\">' + subseccion + '</div>';
-     html +='<ul id=\"botonera\">'; 
-     for(var row=0;row<filas;row++){
-       if( dataDC.validateRow(row)){
-          html +="<li><a  href='"+dataDC.getHref(row)+"' target='_parent'>"+
-          dataDC.getName(row)+"</a></li>";
-       }
+  
+  var DataTable= function(data,url_detail){
+    var HEADER_ROW = 0;
+    this.fields= {
+       id : 0,
+       name: 1,
+       image: 3
+    };
+  
+    var dataDC = data;
+    this.getId = function(row){
+       return dataDC.getValue(row, this.fields.id);
     }
+    this.getHref = function(row){
+       return url_detail+this.getId(row);
+    }
+    this.getNumberOfRows = function(){
+       return dataDC.getNumberOfRows();
+    }
+    this.getName = function(row){
+       return dataDC.getValue(row, this.fields.name);
+    }		
+    this.getImage = function(row){
+       return dataDC.getValue(row, this.fields.image);
+    }
+  
+    this.validateRow = function(row){
+       return true;
+    }
+  
+  }
+  
+  function handleQueryResponseDC(response){
+     var dataDC = new DataTable(response.getDataTable(), url_detail);
+     dataDc.fields = fields;
+    var filas = dataDC.getNumberOfRows();
+  
 
-  html +='</ul>';
-  document.getElementById('content_div').innerHTML = html;
-
-}
-
-function cargaCompleta(){
-  devicesCountry('EspaÃ±a');
+    document.getElementById('content_div').innerHTML = template(dataDC, subseccion);
+  
+  }
 }
